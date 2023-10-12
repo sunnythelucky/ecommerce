6820d43905cba6cf23442880787cde757666aba8
sunny park <sunnyparkk@gmail.com>
2023-10-11 23:52:51 -0400

    Add cookies

--- src/context/index.js
+++ src/context/index.js
@@ -1,11 +1,32 @@
 "use client";
 
-import { createContext, useState } from "react";
+import Cookies from "js-cookie";
+import { createContext, useEffect, useState } from "react";
 
 export const GlobalContext = createContext();
 
 export default function GlobalState({ children }) {
 	const [showNavModal, setShowNavModal] = useState(false);
+	const [commonLoader, setCommonLoader] = useState(false);
+	const [isAuthUser, setIsAuthUser] = useState(null);
+	const [user, setUser] = useState(null);
 
-	return <GlobalContext.Provider value={{ showNavModal, setShowNavModal }}>{children}</GlobalContext.Provider>;
+	useEffect(() => {
+		if (Cookies.get("token") !== undefined) {
+			setIsAuthUser(true);
+			const userData = JSON.parse(localStorage.getItem("user")) || {};
+			setUser(userData);
+		} else {
+			setIsAuthUser(false);
+			setUser({});
+		}
+	}, [Cookies]);
+
+	return (
+		<GlobalContext.Provider
+			value={{ showNavModal, setShowNavModal, commonLoader, setCommonLoader, isAuthUser, setIsAuthUser, user, setUser }}
+		>
+			{children}
+		</GlobalContext.Provider>
+	);
 }
